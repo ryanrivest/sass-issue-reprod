4b41847fb5aa36a46ee99292028348ba387f0ad8
Ryan Rivest <ryan.rivest@cart.com>
2023-12-12 16:25:06 -0800

    updates for reprod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'sass-output-issue';
